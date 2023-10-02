4e06f0ac8f87fa8038166e8ba0f42a54a2411b74
Sathwikkandulaa <kandulasathwik2102@gmail.com>
2023-10-02 12:41:34 +0530

    sath

--- ap.js
+++ ap.js
@@ -109,24 +109,3 @@ nextButton.addEventListener("click", checkAnswer);
 prevButton.addEventListener("click", goBack); 
 
 loadQuestion();
-const logout=()=>{
-
-    const link=document.createElement("a");
-    link.href="/index.html";
-    link.innerText="LogOut";
-    link.style.padding="3px";
-    link.style.borderRadius="5px";
-    link.style.border="1px solid white";
-    link.style.color="black";
-    link.style.textDecoration="none";
-    link.style.margin="10px";
-
-    const ast=document.querySelector(".btn");
-        if(document.querySelector(".btn2").nextSibling===null){
-        ast.appendChild(link);
-        }else{
-            
-            document.querySelector(".btn2").nextSibling.remove();
-        }
-    
-}
